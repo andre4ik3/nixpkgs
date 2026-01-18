fde7d207017dd6ede61777772b4a9e2d3c5960be
andre4ik3 <andre4ik3@fastmail.com>
2026-01-18 22:34:45 +0000

    zapret2: 0.8.3 -> 0.8.6, cleanup and simplify

--- pkgs/by-name/za/zapret2/package.nix
+++ pkgs/by-name/za/zapret2/package.nix
@@ -4,27 +4,40 @@
   lib,
   makeWrapper,
   pkg-config,
-  bind,
   curl,
-  iptables,
+  netcat,
+  gawk,
+  gnugrep,
+  luajit,
+  ipset,
   libcap,
   libmnl,
   libnfnetlink,
   libnetfilter_queue,
-  luajit,
-  nmap,
-  ipset,
+  systemd,
+  zlib,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "zapret2";
-  version = "0.8.3";
+  version = "0.8.6";
+
+  outputs = [
+    "out"
+    "doc"
+  ];
 
   src = fetchFromGitHub {
     owner = "bol-van";
     repo = "zapret2";
-    rev = "v${finalAttrs.version}";
-    hash = "sha256-0mVdqKRTWZcDEQUrAjqK1ujtUAeuqOYtM1pyo+yJxN8=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-gbH8+Hjqt+4itugT2InMjEktUib6s1TtQD+slyz1R94=";
+    leaveDotGit = true;
+    postFetch = ''
+      cd "$out"
+      git rev-parse --short=12 HEAD > COMMIT
+      find "$out" -name .git -print0 | xargs -0 rm -rf
+    '';
   };
 
   nativeBuildInputs = [
@@ -33,113 +46,95 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   buildInputs = [
-    bind
-    curl
-    iptables
+    luajit
+    zlib
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isLinux [
     libcap
     libmnl
     libnfnetlink
     libnetfilter_queue
-    luajit
-    nmap
     ipset
+    systemd
   ];
 
-  # Fix Lua detection and systemd unit
   postPatch = ''
-    # Fix deprecated KillMode
-    sed -i 's/KillMode=none/KillMode=mixed/g' init.d/systemd/zapret2.service
-
-    # Prepare user exclude files
-    cp ipset/zapret-hosts-user-exclude.txt.default ipset/zapret-hosts-user-exclude.txt
-
-    # Help Makefile find luajit
-    sed -i 's|lua5.1|luajit-5.1|g' nfq2/Makefile || true
-    sed -i 's|LUA_PC_NAME := lua|LUA_PC_NAME := luajit|g' nfq2/Makefile || true
+    substituteInPlace "init.d/systemd/nfqws2@.service" \
+      --replace-fail "ExecSearchPath=/opt/zapret2/nfq2" "ExecSearchPath=$out/bin"
   '';
 
-  # Set environment variables for the build
   preBuild = ''
-    export LUA_INCLUDE_DIR=${luajit}/include/luajit-2.1
-    export LUA_LIB_DIR=${luajit}/lib
-    export PKG_CONFIG_PATH="${luajit}/lib/pkgconfig:$PKG_CONFIG_PATH"
+    makeFlagsArray+=("CFLAGS=-DZAPRET_GH_VER=${finalAttrs.src.tag} -DZAPRET_GH_HASH=`cat $src/COMMIT`")
   '';
 
-  # Pass variables to make
   makeFlags = [
-    "PREFIX=${placeholder "out"}/opt/zapret2"
-    "LUA_LIB_NAME=luajit-5.1"
+    (if stdenv.hostPlatform.isLinux then "systemd" else "bsd")
+    "TGT=${placeholder "out"}/bin"
   ];
 
   installPhase = ''
-    mkdir -p $out/opt/zapret2/{ip2net,mdig,nfq2,ipset,blockcheck2.d/{custom,standard},files/fake,common,lua,init.d/sysv}
-    mkdir -p $out/etc/{systemd/system,sysusers.d}
-    mkdir -p $out/share/{doc/zapret2,licenses/zapret2}
-    mkdir -p $out/bin
+    runHook preInstall
 
-    # Install binaries (built from source)
-    install -Dm755 nfq2/nfqws2  -t $out/opt/zapret2/nfq2/
-    install -Dm755 ip2net/ip2net -t $out/opt/zapret2/ip2net/
-    install -Dm755 mdig/mdig    -t $out/opt/zapret2/mdig/
+    ZAPRET_BASE="$out/share/zapret2"
+    ZAPRET_RW="/etc/zapret2"
 
-    # Install scripts and configs
-    install -Dm755 blockcheck2.sh -t $out/opt/zapret2/
-    install -Dm644 blockcheck2.d/custom/* -t $out/opt/zapret2/blockcheck2.d/custom/
-    install -Dm644 blockcheck2.d/standard/* -t $out/opt/zapret2/blockcheck2.d/standard/
-    install -Dm644 files/fake/* -t $out/opt/zapret2/files/fake/
-    install -Dm644 common/* -t $out/opt/zapret2/common/
-    install -Dm644 lua/* -t $out/opt/zapret2/lua/
+    mkdir -p "$ZAPRET_BASE"
+    for spec in "blockcheck2.sh" "blockcheck2.d" "common" "ipset" "lua"; do
+      cp -R "$spec" "$ZAPRET_BASE/"
+    done
 
-    # Install ipset files
-    for f in ipset/*.sh; do [ -f "$f" ] && install -Dm755 "$f" -t $out/opt/zapret2/ipset/; done
-    for f in ipset/*.txt ipset/*.default; do [ -f "$f" ] && install -Dm644 "$f" -t $out/opt/zapret2/ipset/; done
+    pushd docs
+    mkdir -p "$doc/share/doc/zapret2"
+    cp *.md changes*.txt "$doc/share/doc/zapret2"
+    popd
 
-    # Install init scripts
-    install -Dm755 init.d/sysv/{functions,zapret2} -t $out/opt/zapret2/init.d/sysv/
-    install -Dm644 init.d/systemd/* -t $out/etc/systemd/system/
+    wrapProgram "$ZAPRET_BASE/blockcheck2.sh" \
+      --set ZAPRET_BASE "$ZAPRET_BASE" \
+      --set ZAPRET_RW "$ZAPRET_RW" \
+      --set NFQWS2 "$out/bin/nfqws2" \
+      --set DVTWS2 "$out/bin/dvtws2" \
+      --set MDIG "$out/bin/mdig" \
+      --prefix "PATH" : "${
+        lib.makeBinPath [
+          curl
+          netcat
+          # don't wrap with iptables/nftables so that it uses the one from the
+          # system path (depending on the networking firewall backend)
+        ]
+      }"
 
-    # Create sysusers config
-    echo "u zapret2 - \"zapret2 Anti-DPI software\"" > $out/etc/sysusers.d/zapret2.conf
+    for file in "$ZAPRET_BASE/ipset"/*.sh; do
+      if [ -x "$file" ]; then
+        wrapProgram "$file" \
+          --set ZAPRET_BASE "$ZAPRET_BASE" \
+          --set ZAPRET_RW "$ZAPRET_RW" \
+          --prefix "PATH" : "${
+            lib.makeBinPath [
+              gawk
+              gnugrep
+            ]
+          }"
+      fi
+    done
 
-    # Create symlinks
-    ln -s $out/opt/zapret2/init.d/sysv/zapret2 $out/bin/zapret2
-    ln -s $out/opt/zapret2/ip2net/ip2net $out/bin/ip2net
-    ln -s $out/opt/zapret2/mdig/mdig    $out/bin/mdig
-    ln -s $out/opt/zapret2/nfq2/nfqws2  $out/bin/nfqws2
+    ln -s "$ZAPRET_BASE/blockcheck2.sh" "$out/bin/blockcheck2"
 
-    # Install docs
-    install -Dm644 docs/*.* -t $out/share/doc/zapret2/
-    install -Dm644 docs/LICENSE.txt -T $out/share/licenses/zapret2/LICENSE
+    ${lib.optionalString stdenv.hostPlatform.isLinux ''
+      install -Dm644 "init.d/systemd/nfqws2@.service" -t "$out/lib/systemd/system"
+    ''}
 
-    # Install main config
-    install -Dm644 config.default -T $out/opt/zapret2/config
-  '';
-
-  postFixup = ''
-    wrapProgram $out/bin/ip2net --prefix PATH : ${lib.makeBinPath [ bind ]}
-    wrapProgram $out/bin/mdig    --prefix PATH : ${
-      lib.makeBinPath [
-        bind
-        nmap
-      ]
-    }
-    wrapProgram $out/bin/nfqws2  --prefix PATH : ${
-      lib.makeBinPath [
-        iptables
-        ipset
-        nmap
-        curl
-        luajit
-      ]
-    }
+    runHook postInstall
   '';
 
   meta = with lib; {
     description = "Anti-DPI software for bypassing DPI systems";
     homepage = "https://github.com/bol-van/zapret2";
     license = licenses.mit;
-    platforms = platforms.linux;
+    platforms = platforms.unix;
+    # Darwin lacks ipdivert in kernel (making dvtws2 unusable) and zapret2
+    # doesn't include tpws
+    broken = stdenv.hostPlatform.isDarwin;
     maintainers = [ ];
-    mainProgram = "nfqws2";
+    mainProgram = if stdenv.hostPlatform.isLinux then "nfqws2" else "dvtws2";
   };
 })
